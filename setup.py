2f366ea0a1e713165ced30cd678359ee67d2ed39
Richard Tier <rikatee@gmail.com>
2018-03-02 14:36:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 def get_version():
     pattern = re.compile(r'__version__\s+=\s+(.*)')
 
-    with open('directory_client/version.py', 'rb') as src:
+    with open('directory_client_core/version.py', 'rb') as src:
         return str(ast.literal_eval(
             pattern.search(src.read().decode('utf-8')).group(1)
         ))
