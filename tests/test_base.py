700630c6bbc52598b2dadcad3a9cb92d846e7595
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-03-15 15:03:17 +0000

    sanity check for base url

--- tests/test_base.py
+++ tests/test_base.py
@@ -14,7 +14,7 @@ class BaseAPIClientTest(TestCase):
 
     def setUp(self):
         self.client = BaseAPIClient(
-            base_url='https://example.com', api_key='test'
+            base_url='https://example.com/', api_key='test'
         )
 
     @stub_request('https://example.com/test', 'post')
