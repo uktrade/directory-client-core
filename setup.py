dd2054c6a75404d4a1ff94d22ab105e7b04f4c95
richtier <rikatee@gmail.com>
2019-01-10 08:13:59 +0000

    Upgrade requests for security vulnerability fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_client_core',
-    version='4.3.0',
+    version='4.3.1',
     url='https://github.com/uktrade/directory-client-core',
     license='MIT',
     author='Department for International Trade',
@@ -16,7 +16,7 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
-        'requests>=2.18.4,<3.0.0',
+        'requests>=2.21.0,<3.0.0',
         'monotonic>=1.2,<3.0',
         'sigauth>=4.0.1,<5.0.0',
         'django>=1.9,<2.0a1',
