854848dec9f7aa2075b76f2f48606f48b42d300d
richtier <rikatee@gmail.com>
2019-01-23 09:01:56 +0000

    Improve auth negotiator

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,35 +1,38 @@
+import pytest
+
 from directory_client_core import authentication
 
 
+def test_authentication_negotiator_neither():
+    with pytest.raises(AssertionError):
+        authentication.AuthenticatorNegotiator()
+
+
 def test_authentication_negotiator_sso():
-    authenticator = authentication.AuthenticatorNegotiator(
-        sso_session_id='123', bearer_token=None
-    )
+    authenticator = authentication.AuthenticatorNegotiator(sso_session_id='12')
 
     assert isinstance(authenticator, authentication.SessionSSOAuthenticator)
-    assert authenticator.value == '123'
+    assert authenticator.value == '12'
 
 
 def test_authentication_negotiator_bearer():
-    authenticator = authentication.AuthenticatorNegotiator(
-        sso_session_id=None, bearer_token='123'
-    )
+    authenticator = authentication.AuthenticatorNegotiator(bearer_token='12')
 
     assert isinstance(authenticator, authentication.BearerAuthenticator)
-    assert authenticator.value == '123'
+    assert authenticator.value == '12'
 
 
 def test_bearer_authenticator():
-    authenticator = authentication.BearerAuthenticator('123')
+    authenticator = authentication.BearerAuthenticator('12')
 
     assert authenticator.headers == {
-        'Authorization': 'Bearer 123'
+        'Authorization': 'Bearer 12'
     }
 
 
 def test_sso_authenticator():
-    authenticator = authentication.SessionSSOAuthenticator('123')
+    authenticator = authentication.SessionSSOAuthenticator('12')
 
     assert authenticator.headers == {
-        'Authorization': 'SSO_SESSION_ID 123'
+        'Authorization': 'SSO_SESSION_ID 12'
     }
