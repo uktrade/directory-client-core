6a949b5efa150c726804429152473c77eeb5fd94
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-07-15 13:49:42 +0100

    update versions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_client_core',
-    version='5.1.1',
+    version='6.0.0',
     url='https://github.com/uktrade/directory-client-core',
     license='MIT',
     author='Department for International Trade',
