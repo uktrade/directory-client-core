ef30f61bc0f44d4aa7258df274d5bdc5778f65e6
Richard Tier <rikatee@gmail.com>
2019-07-18 12:16:10 +0100

    Update setup.py
    Support django 1.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_client_core',
-    version='6.0.1',
+    version='6.1.0',
     url='https://github.com/uktrade/directory-client-core',
     license='MIT',
     author='Department for International Trade',
@@ -19,7 +19,7 @@ setup(
         'requests>=2.21.0,<3.0.0',
         'monotonic>=1.2,<3.0',
         'sigauth>=4.0.1,<5.0.0',
-        'django>=2.2,<3.0a1',
+        'django>=1.11.22,<3.0a1',
         'w3lib>=1.19.0<2.0.0',
     ],
     extras_require={
@@ -42,6 +42,9 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
